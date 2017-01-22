4d098b6c14c04617484469c2307fa695fd567646
ukahoot <gotzathe@gmail.com>
2017-01-22 17:46:22 -0600

    Add Kahoot Token Request Object

---
+++ tokenserver/src/KahootTokenRequest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UKahoot {
+	public class KahootTokenRequest {
+		public HttpResponseMessage Response;
+		public HttpContent Content;
+		public string Result;
+
+		public string RequestUri;
+		private HttpClient Client;
+		public KahootTokenRequest() {
+			Client = new HttpClient();
+			Client.BaseAddress = new Uri(Util.TOKEN_ENDPOINT);
+			// Add headers to the request
+			Client.DefaultRequestHeaders.Referrer = new Uri(Util.ENDPOINT_URI);
+			Client.DefaultRequestHeaders.Host = Util.ENDPOINT_HOST;
+		}
+		public async Task GetToken(int PID) {
+			// Send the HTTPS request to the Kahoot token endpoint
+			RequestUri = Util.GetTokenRequestUri(PID);
+			Response = await Client.GetAsync(RequestUri);
+			Content  = Response.Content;
+			Result   = await Content.ReadAsStringAsync();
+		}
+	}
+}
--- tokenserver/src/Util.cs
+++ tokenserver/src/Util.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 namespace UKahoot {
 	public static class Util {
+		public const string TOKEN_ENDPOINT = "https://kahoot.it/reserve/session/";
+		public const string ENDPOINT_URI = "https://kahoot.it/";
+		public const string ENDPOINT_HOST = "kahoot.it";
 		public static byte[] GetBytes(string txt) {
 			return Encoding.ASCII.GetBytes(txt);
 		}
@@ -11,6 +14,13 @@ namespace UKahoot {
 			public static byte[] RequestError = GetBytes("<html><body><h1>Request Processing Error</h1></body></html>");
 			public static byte[] InvalidQueryString = GetBytes("<html><body><h1>Invalid Query String</h1></body></html>");
 			public static byte[] InvalidRequest = GetBytes("<html><body><h1>Invalid Request</h1></body></html>");
+		}
+		public static int GetKahootTime() {
+			var timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+    		return (int)timeSpan.TotalSeconds;
+		}
+		public static string GetTokenRequestUri(int PID) {
+			return TOKEN_ENDPOINT + "/" + PID + "/?" + GetKahootTime();
 		}
 	}
 }
