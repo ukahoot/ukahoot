9ddcff831747998d6bba376d0e102c49c5d55da9
gotzathe <gotzathe@gmail.com>
2017-03-12 16:54:34 -0500

    Change loader browser checking

--- site/src/loader.js
+++ site/src/loader.js
@@ -1,18 +1,17 @@
 (function() {
     "use strict";
-    {
-		var supported = true;
-		try {
-			class Test {}
-		} catch (e) {
-			supported = false;
-		}
-		if (typeof fetch === "undefined") supported = false;
-		if (!supported) {
-			alert('Your browser does not support Ukahoot!\nMake sure you are using the latest version of Chrome.');
-			return;
-		}
-	}
+    // Check if browser supports ES6 and fetch
+    var supported = true;
+    try {
+        class Test {}
+    } catch (e) {
+        supported = false;
+    }
+    if (typeof fetch === "undefined") supported = false;
+    if (!supported) {
+        alert('Your browser does not support Ukahoot!\nMake sure you are using the latest version of Chrome.');
+        return;
+    }
     // Small module system to load JS source files
     class UKahootModule {
         constructor(filename, name) {
