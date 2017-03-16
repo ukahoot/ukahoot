92df709f69e51617b150dd7161abebc69f57b28d
gotzathe <lolurbadm9@gmail.com>
2017-03-16 17:54:22 -0500

    Add electron window configurations and ico

--- site/src/electron/electron.js
+++ site/src/electron/electron.js
@@ -9,12 +9,23 @@ const LOAD_PATH = url.format({
     protocol: "file:",
     slashes: true
 });
+var WINDOW_OPTS = {
+    resizable: true,
+    darkTheme: true,
+    javascript: true,
+    center: true,
+    minWidth: 800,
+    minHeight: 600,
+    width: 1280,
+    height: 960,
+    title: "ukahoot",
+    icon: path.resolve("assets/icon.png")
+}
 console.log("loading file " + LOAD_PATH);
 let bw = null; // The browser window to run the app in
 let init = () => {
-    bw = new electron.BrowserWindow;
+    bw = new electron.BrowserWindow(WINDOW_OPTS);
     bw.loadURL(LOAD_PATH);
-    bw.webContents.openDevTools();
     bw.on('closed', () => bw = null);
 }
 electron.app.on('ready', init);
