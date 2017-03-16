d10bb9661b5034dda1a39975414c24e93c14f264
gotzathe <lolurbadm9@gmail.com>
2017-03-16 00:18:47 -0500

    Fix ThemeEditor CSS

--- site/src/interface/SiteInterface/ThemeEditor.js
+++ site/src/interface/SiteInterface/ThemeEditor.js
@@ -1,9 +1,11 @@
 class ThemeEditor {
     show() {
+        $("#overlay").fadeIn(250);
         $("#theme-editor").fadeIn(250);
     }
     hide() {
         $("#theme-editor").fadeOut(250);
+        $("#overlay").fadeOut(250);
     }
     constructor() { }
 }
