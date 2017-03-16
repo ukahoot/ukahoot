8c6d9d92092b13a86b6cec85be8141757e2da8a4
gotzathe <lolurbadm9@gmail.com>
2017-03-16 17:31:27 -0500

    Add style to Electron Interface

--- site/src/electron/ElectronInterface.js
+++ site/src/electron/ElectronInterface.js
@@ -1,5 +1,7 @@
 class ElectronInterface extends SiteInterface {
     constructor() {
         super();
+        this.style = new Style(ElectronInterface.STYLE_PATH);
     }
 }
+ElectronInterface.STYLE_PATH = "../interface/SiteInterface/siteInterface.css";
