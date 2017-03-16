5b55cb96d43146c9c894d6e68782a501c26543fd
gotzathe <lolurbadm9@gmail.com>
2017-03-16 00:23:01 -0500

    Add theme hide/show buttons for theme editor

--- site/src/interface/SiteInterface/ThemeEditor.js
+++ site/src/interface/SiteInterface/ThemeEditor.js
@@ -7,5 +7,11 @@ class ThemeEditor {
         $("#theme-editor").fadeOut(250);
         $("#overlay").fadeOut(250);
     }
-    constructor() { }
+    constructor() {
+        var me = this;
+        document.getElementById("show-theme").addEventListener("click",
+                                                                                                                    me.show);
+        document.getElementById("hide-theme").addEventListener("click",
+                                                                                                                    me.hide);
+    }
 }
