333fa2ae30486f6b8225dbe8c8cb2f93201d9881
gotzathe <lolurbadm9@gmail.com>
2017-03-16 19:58:34 -0500

    Create basic electron building system

--- ukahoot/src/electron/electron.js
+++ ukahoot/src/electron/electron.js
@@ -1,15 +1,7 @@
 const electron = require("electron");
-const path = require("path");
 const url = require("url");
-const process = require('process');
 const os = require("os");
 
-process.chdir(".."); // Go back one directory
-const LOAD_PATH = url.format({
-    pathname: path.resolve("ukahoot/src/electron/app.html"),
-    protocol: "file:",
-    slashes: true
-});
 var WINDOW_OPTS = {
     resizable: true,
     darkTheme: true,
@@ -20,15 +12,15 @@ var WINDOW_OPTS = {
     width: 1280,
     height: 960,
     title: "ukahoot",
-    icon: path.resolve("assets/icon.png")
+    icon: "icon.png"
 }
 if (os.platform() === "win32") // Use ICO file on windows
-    WINDOW_OPTS.icon = path.resolve("assets/icon.ico");
-console.log("loading file " + LOAD_PATH);
+    WINDOW_OPTS.icon = "icon.ico"
 let bw = null; // The browser window to run the app in
 let init = () => {
     bw = new electron.BrowserWindow(WINDOW_OPTS);
-    bw.loadURL(LOAD_PATH);
+    bw.loadURL('file://' + __dirname + '/app.html');
+    bw.webContents.openDevTools();
     bw.on('closed', () => bw = null);
 }
 electron.app.on('ready', init);
