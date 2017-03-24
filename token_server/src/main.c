84b63b552f6293b30d510e6422441936d8fc001b
gotzathe <lolurbadm9@gmail.com>
2017-03-23 22:27:07 -0500

    Add message read test

--- token_server/src/main.c
+++ token_server/src/main.c
@@ -10,4 +10,8 @@ int main(int argc, char* argv[]) {
 	request_connect(r);
 	printf("sending message\n");
 	request_write_str(r, "GET / HTTP/1.1\r\n\r\n");
+	printf("reading message\n");
+	char* res = malloc(512);
+	request_read(r, res, 512);
+	printf("%s\n", res);
 };
