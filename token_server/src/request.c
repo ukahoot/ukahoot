cf430b92e7c5bdad52cc7749f760bbfb9b7f94bd
gotzathe <lolurbadm9@gmail.com>
2017-03-23 21:23:33 -0500

    Uncomment request initialization

--- token_server/src/request.c
+++ token_server/src/request.c
@@ -20,12 +20,12 @@ typedef struct {
 req* init_request(void) {
     req* request;
     request = malloc(sizeof(req)); // Allocate a request on the heap
-    request->sockfd = socket(AF_INET, SOCK_STREAM, 0);/*
+    request->sockfd = socket(AF_INET, SOCK_STREAM, 0);
     request->context = SSL_CTX_new(SSLv23_client_method());
     request->conn = SSL_new(request->context);
     SSL_set_fd(request->conn, request->sockfd);
     request->serv = gethostbyname(KAHOOT_HOST_URI);
     request->addr.sin_family = AF_INET;
-    request->addr.sin_addr.s_addr = request->serv->h_addr;*/
+    request->addr.sin_addr.s_addr = request->serv->h_addr;
     return request;
 };
