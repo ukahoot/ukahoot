e4f214cbaa1f167479087ed692e3f0bafac62696
gotzathe <gotzathe@gmail.com>
2017-03-26 21:34:24 -0500

    Change failure constants

--- token_server/src/kahoot.h
+++ token_server/src/kahoot.h
@@ -1,6 +1,14 @@
 #define KAHOOT_SSL_PORT 443
 #define KAHOOT_HOST_URI "kahoot.it"
 
+#define RES_FAIL "HTTP/1.1 403 FORBIDDEN\r\n\
+Access-Control-Allow-Origin: *\r\n\
+Content-Type: application/json\r\n\
+Server: ukahoot_token_server\r\n\
+Keep-Alive: timeout=15,max=100\r\n\
+\r\n"
+#define RES_FAIL_LEN 152
+
 static const char* KAHOOT_TOKEN_GET = "GET /reserve/session/";
 static int KAHOOT_GET_LEN = 22;
 static const char* KAHOOT_TOKEN_HEADERS = " HTTP/1.1\r\n\
@@ -21,14 +29,6 @@ Keep-Alive: timeout=15,max=100\r\n\
 Content-Length: 166\r\n\
 \r\n";
 static const int RES_SUCCESS_HEADERS_LEN = 166;
-
-static const char* RES_FAIL = "HTTP/1.1 403 FORBIDDEN\r\n\
-Access-Control-Allow-Origin: *\r\n\
-Content-Type: application/json\r\n\
-Server: ukahoot_token_server\r\n\
-Keep-Alive: timeout=15,max=100\r\n\
-Content-Length: 173\r\n\r\n";
-static const int RES_FAIL_LEN = 173;
 
 char* get_req_headers(char*);
 char* get_pid_query(char*);
