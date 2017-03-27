406d2939491f7dabdc4534e98043cfb4bf771620
gotzathe <gotzathe@gmail.com>
2017-03-26 21:35:16 -0500

    Change success header constants

--- token_server/src/kahoot.h
+++ token_server/src/kahoot.h
@@ -9,6 +9,15 @@ Keep-Alive: timeout=15,max=100\r\n\
 \r\n"
 #define RES_FAIL_LEN 152
 
+#define RES_SUCCESS_HEADERS "HTTP/1.1 200 OK\r\n\
+Access-Control-Allow-Origin: *\r\n\
+Content-Type: application/json\r\n\
+Server: ukahoot_token_server\r\n\
+Keep-Alive: timeout=15,max=100\r\n\
+Content-Length: 166\r\n\
+\r\n"
+#define RES_SUCCESS_HEADERS_LEN 166
+
 static const char* KAHOOT_TOKEN_GET = "GET /reserve/session/";
 static int KAHOOT_GET_LEN = 22;
 static const char* KAHOOT_TOKEN_HEADERS = " HTTP/1.1\r\n\
@@ -20,15 +29,6 @@ Referer: https://kahoot.it/\r\n\
 Accept-Language: en-US,en;q=0.8\r\n\
 \r\n";
 static const int KAHOOT_HEADERS_SIZE = 191;
-
-static const char* RES_SUCCESS_HEADERS = "HTTP/1.1 200 OK\r\n\
-Access-Control-Allow-Origin: *\r\n\
-Content-Type: application/json\r\n\
-Server: ukahoot_token_server\r\n\
-Keep-Alive: timeout=15,max=100\r\n\
-Content-Length: 166\r\n\
-\r\n";
-static const int RES_SUCCESS_HEADERS_LEN = 166;
 
 char* get_req_headers(char*);
 char* get_pid_query(char*);
