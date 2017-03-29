c12123e42d865d0d7f8632448ec738c63789134b
gotzathe <lolurbadm9@gmail.com>
2017-03-29 00:03:08 -0500

    Add color header file

---
+++ token_server/src/colors.h
@@ -0,0 +1,10 @@
+// Color macros for colored console output
+#pragma once
+#define CLR_RED   "\x1B[31m"
+#define CLR_GRN   "\x1B[32m"
+#define CLR_YEL   "\x1B[33m"
+#define CLR_BLU   "\x1B[34m"
+#define CLR_MAG   "\x1B[35m"
+#define CLR_CYN   "\x1B[36m"
+#define CLR_WHT   "\x1B[37m"
+#define CLR_RESET "\x1B[0m"
